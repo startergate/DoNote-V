1101813a4f31442da6b069721877156e66ff15e4
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-24 23:41:43 +0900

    [ADD] Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import dLogo from './donoteLogo.png'
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={dLogo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Nyan <code>src/App.tsx</code> and Nyan to Nyan.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/startergate"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Nyan
         </a>
       </header>
     </div>
