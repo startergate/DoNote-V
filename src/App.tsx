ab590d90dd46ba9f753b95df41899e7741e06fbc
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-25 20:27:00 +0900

    로그인

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import dLogo from './donoteLogo.png'
+import logo from './donoteLogo.png'
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={dLogo} className="App-logo" alt="logo" />
-        <p>
-          Nyan <code>src/App.tsx</code> and Nyan to Nyan.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/startergate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Nyan
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <header className="App-header login">
+                <img src={logo} className="App-logo title" alt="logo" />
+                <a
+                    className="loginBtn"
+                    href="http://sid.donote.co:3000/user/login"
+                >SID로 로그인</a>
+            </header>
+        </div>
+    );
+};
 
 export default App;
