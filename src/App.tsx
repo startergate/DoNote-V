8993eef7b5aa68d67e0671176dcf5ab8dcd7a85c
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-26 22:54:29 +0900

    revert to old

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,19 @@ import { hot } from 'react-hot-loader';
 import logo from './donoteLogo.png'
 import './App.css';
 
-class App extends React.Component {
-  constructor(props: any) {
-    super(props);
+const App: React.FC = () => {
 
-    this.state = {}
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header login">
-          <img src={logo} className="App-logo title" alt="logo" />
-          <a
-            className="loginBtn"
-            href="http://sid.donote.co:3000/user/login"
-          >SID로 로그인</a>
-        </header>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <header className="App-header login">
+        <img src={logo} className="App-logo title" alt="logo" />
+        <a
+          className="loginBtn"
+          href="http://sid.donote.co:3000/user/login"
+        >SID로 로그인</a>
+      </header>
+    </div>
+  );
+};
 
 export default App;
