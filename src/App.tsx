95e32735fe9682c243959e1bae495d0a01dcfd7c
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-26 23:42:55 +0900

    [ADD] Router 구조 도입

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React, { Fragment } from 'react';
+import { createBrowserHistory } from 'history';
 import { hot } from 'react-hot-loader';
+import { Router, Route, Switch } from 'react-router-dom';
 
+import {} from 'pages';
 import logo from './donoteLogo.png'
 import './App.css';
 
-const App: React.FC = () => {
+const history = createBrowserHistory();
 
+
+const App: React.FC = () => {
   return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/note"/>
+        <Route path="/manage"/>
+        <Route path="/"/>
+      </Switch>
+    </Router>
+  )
+  /*return (
     <div className="App">
       <header className="App-header login">
         <img src={logo} className="App-logo title" alt="logo" />
@@ -16,7 +30,7 @@ const App: React.FC = () => {
         >SID로 로그인</a>
       </header>
     </div>
-  );
+  );*/
 };
 
 export default App;
